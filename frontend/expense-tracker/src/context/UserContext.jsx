23d7f54702ad7d5810b05fb29e715977cb36c2be
harshrohada <harshrohada@gmail.com>
2025-07-25 16:18:09 +0530

    chnages

--- frontend/expense-tracker/src/context/UserContext.jsx
+++ frontend/expense-tracker/src/context/UserContext.jsx
@@ -1,6 +1,6 @@
 import React,{createContext,useState} from 'react';
 
-export const UserContext=createContext(null);
+export const UserContext=createContext();
 
 const UserProvider=({children})=>{
     const [user,setUser]=useState();
